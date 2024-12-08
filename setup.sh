7222ef2d062659ca37ea95d260866e70f879f052
kevinnguyenhoang91 <kevin.nguyenhoang91@gmail.com>
2024-12-08 21:59:19 +0700

    feat: add devcontainer setup to Makefile
    - Added a new 'devcontainer' target to the Makefile for setting up the
      development container.
    - Updated the 'setup.sh' script to call 'make devcontainer' instead of
      directly installing dependencies and configurations.
    - Commented out the Homebrew installation and package setup in the
      'setup.sh' script.
    - Commented out the oh-my-zsh and Nerd Fonts installation in the
      'setup.sh' script.

--- setup.sh
+++ setup.sh
@@ -1,33 +1,32 @@
 #!/bin/zsh
 
-# Install Homebrew
-if ! command -v brew &> /dev/null; then
-    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-fi
-[ -f /home/linuxbrew/.linuxbrew/bin/brew ] && eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
+# mkdir -p ~/.zsh
+# git clone https://github.com/catppuccin/zsh-syntax-highlighting.git
+# cp -RLf zsh-syntax-highlighting/themes/catppuccin_mocha-zsh-syntax-highlighting.zsh ~/.zsh/
+# rm -rf zsh-syntax-highlighting
+#
+# git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+#
+# # Nerd Fonts
+# mkdir -p "${HOME}/.local/share/fonts"
+# curl -fsSL https://raw.githubusercontent.com/getnf/getnf/main/install.sh | bash
+# getnf -i 'Noto,FiraMono,FiraCode,Meslo'
+#
+# # atuin
+# if command -v atuin &> dev/null; then
+#   eval "$(atuin init zsh)"
+# fi
+# ||||||| parent of 64e0d5671e08 (feat: add devcontainer setup to Makefile)
+# mkdir -p ~/.zsh
+# git clone https://github.com/catppuccin/zsh-syntax-highlighting.git
+# cp -RLf zsh-syntax-highlighting/themes/catppuccin_mocha-zsh-syntax-highlighting.zsh ~/.zsh/
+# rm -rf zsh-syntax-highlighting
+#
+# git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+#
+# # Nerd Fonts
+# mkdir -p "${HOME}/.local/share/fonts"
+# curl -fsSL https://raw.githubusercontent.com/getnf/getnf/main/install.sh | bash
+# getnf -i 'Noto,FiraMono,FiraCode,Meslo'
 
-# Install Homebrew packages
-cd brew && brew bundle
-cd "$HOME"
-
-# oh-my-zsh
-sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
-
-mkdir -p ~/.zsh
-git clone https://github.com/catppuccin/zsh-syntax-highlighting.git
-cp -RLf zsh-syntax-highlighting/themes/catppuccin_mocha-zsh-syntax-highlighting.zsh ~/.zsh/
-rm -rf zsh-syntax-highlighting
-
-git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
-
-# Nerd Fonts
-mkdir -p "${HOME}/.local/share/fonts"
-curl -fsSL https://raw.githubusercontent.com/getnf/getnf/main/install.sh | bash
-getnf -i 'Noto,FiraMono,FiraCode,Meslo'
-
-# atuin
-if command -v atuin &> dev/null; then
-  eval "$(atuin init zsh)"
-fi
+make devcontainer
