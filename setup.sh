b65879b33e2418dc55088b77280f2e4afbe0b7cd
kevinnguyenhoang91 <kevin.nguyenhoang91@gmail.com>
2024-12-08 21:59:19 +0700

    feat: add devcontainer setup to Makefile
    - Added a new 'devcontainer' target to the Makefile for setting up the
      development container.
    - Updated the 'setup.sh' script to call 'make devcontainer' instead of
      directly installing dependencies and configurations.
    - Commented out the Homebrew installation and package setup in the
      'setup.sh' script.
    - Commented out the oh-my-zsh and Nerd Fonts installation in the
      'setup.sh' script.

--- setup.sh
+++ setup.sh
@@ -1,26 +1,28 @@
 #!/bin/zsh
 
 # Install Homebrew
-if ! command -v brew &> /dev/null; then
-    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-fi
-[ -f /home/linuxbrew/.linuxbrew/bin/brew ] && eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
+# if ! command -v brew &> /dev/null; then
+#     /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+# fi
+# [ -f /home/linuxbrew/.linuxbrew/bin/brew ] && eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
 
 # Install Homebrew packages
-cd brew && brew bundle
-cd "$HOME"
+# cd brew && brew bundle
+# cd "$HOME"
 
-# oh-my-zsh
-sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+# # oh-my-zsh
+# sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+# git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+# git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+#
+# mkdir -p ~/.zsh
+# git clone https://github.com/catppuccin/zsh-syntax-highlighting.git
+# cp -RLf zsh-syntax-highlighting/themes/catppuccin_mocha-zsh-syntax-highlighting.zsh ~/.zsh/
+# rm -rf zsh-syntax-highlighting
+#
+# # Nerd Fonts
+# mkdir -p "${HOME}/.local/share/fonts"
+# curl -fsSL https://raw.githubusercontent.com/getnf/getnf/main/install.sh | bash
+# getnf -i 'Noto,FiraMono,FiraCode'
 
-mkdir -p ~/.zsh
-git clone https://github.com/catppuccin/zsh-syntax-highlighting.git
-cp -RLf zsh-syntax-highlighting/themes/catppuccin_mocha-zsh-syntax-highlighting.zsh ~/.zsh/
-rm -rf zsh-syntax-highlighting
-
-# Nerd Fonts
-mkdir -p "${HOME}/.local/share/fonts"
-curl -fsSL https://raw.githubusercontent.com/getnf/getnf/main/install.sh | bash
-getnf -i 'Noto,FiraMono,FiraCode'
+make devcontainer
