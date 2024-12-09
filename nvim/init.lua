4f9a03b462bda5806da7c796d9c45c7f84262244
kevinnguyenhoang91 <kevin.nguyenhoang91@gmail.com>
2024-12-09 20:32:07 +0700

    feat(init.lua): add color scheme and auto-open Neo-tree
    - Set the color scheme to 'catppuccin-frappe' for a better visual
      experience.
    - Automatically open Neo-tree when Neovim starts to improve workflow
      efficiency.

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,3 +1,8 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
+
+-- Color scheme
 vim.cmd("colorscheme catppuccin-frappe")
+
+-- Open Neo-tree automatically when Neovim starts
+vim.cmd("autocmd VimEnter * Neotree")
