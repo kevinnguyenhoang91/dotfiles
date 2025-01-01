de0af2aa57d84621af7ccef9851e057310130492
kevinnguyenhoang91 <kevin.nguyenhoang91@gmail.com>
2025-01-01 12:14:25 +0700

    feat: update plugins and configurations
    - Added .neoconf.json for neodev and neoconf configurations.
    - Updated init.lua to include an optional colorscheme.
    - Updated lazy-lock.json with new plugin commits.
    - Modified lazyvim.json to remove and add specific extras.
    - Added blink.lua plugin configuration.
    - Updated copilot.lua with new settings.
    - Removed telescope.lua plugin configuration.

--- nvim/init.lua
+++ nvim/init.lua
@@ -3,6 +3,7 @@ require("config.lazy")
 
 -- Color scheme
 vim.cmd("colorscheme catppuccin-frappe")
+-- vim.cmd("colorscheme tokyonight")
 
 -- Open Neo-tree automatically when Neovim starts
 -- vim.cmd("autocmd VimEnter * Neotree")
--- nvim/lua/plugins/copilot.lua
+++ nvim/lua/plugins/copilot.lua
@@ -3,7 +3,7 @@ return {
     "CopilotC-NVim/CopilotChat.nvim",
     opts = {
       auto_insert_mode = false,
-      insert_at_end = true,
+      insert_at_end = false,
       window = {
         width = 0.25,
       },
@@ -23,4 +23,28 @@ return {
       },
     },
   },
+  -- {
+  --   "zbirenbaum/copilot.lua",
+  --   config = function()
+  --     require("copilot").setup({
+  --       panel = {
+  --         enabled = false,
+  --       },
+  --       suggestion = {
+  --         enabled = false,
+  --       },
+  --       filetypes = {
+  --         yaml = true,
+  --         markdown = false,
+  --         help = false,
+  --         gitcommit = false,
+  --         gitrebase = false,
+  --         hgcommit = false,
+  --         svn = false,
+  --         cvs = false,
+  --         ["."] = true,
+  --       },
+  --     })
+  --   end,
+  -- },
 }
