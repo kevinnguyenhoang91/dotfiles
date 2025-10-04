5f92944d648c35bc24714f156de1898c5c22348c
kevinnguyenhoang91 <kevin.nguyenhoang91@gmail.com>
2025-10-04 13:49:36 +0700

    Update dotfiles

--- nvim/lua/plugins/themes.lua
+++ nvim/lua/plugins/themes.lua
@@ -2,4 +2,7 @@ return {
   { "rebelot/kanagawa.nvim" },
   { "kvrohit/substrata.nvim" },
   { "nyoom-engineering/oxocarbon.nvim" },
+  { "catppuccin", opts = {
+    transparent_background = true,
+  } },
 }
